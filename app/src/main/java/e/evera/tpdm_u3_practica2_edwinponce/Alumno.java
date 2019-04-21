2c5c452c162526362b2f6976ead15a813f553607
edwin97e <edwin97e@gmail.com>
2019-04-20 20:59:43 -0600

    first commit

---
+++ app/src/main/java/e/evera/tpdm_u3_practica2_edwinponce/Alumno.java
@@ -0,0 +1,12 @@
+package e.evera.tpdm_u3_practica2_edwinponce;
+
+public class Alumno {
+    String nombre, domicilio, edad, telefono;
+
+    public Alumno(String nom, String dom, String ed, String tel){
+        nombre = nom;
+        domicilio = dom;
+        edad = ed;
+        telefono = tel;
+    }
+}
---
+++ app/src/main/java/e/evera/tpdm_u3_practica2_edwinponce/Docente.java
@@ -0,0 +1,13 @@
+package e.evera.tpdm_u3_practica2_edwinponce;
+
+public class Docente {
+    String nombre, rfc, domicilio, telefono;
+
+    public Docente(String nombre, String domicilio, String rfc, String telefono) {
+        this.nombre = nombre;
+        this.rfc = rfc;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+}
+
